694cd0d75741f2e528f62120fae52db81eabb75b
zakharfreedomLawyer <107808581+zakharfreedomLawyer@users.noreply.github.com>
2022-09-15 10:50:49 +0300

    Initial Commit

---
+++ Netology_IB_Instruments_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netology_IB_Instruments_1
+//
+//  Created by Захар Кисляк on 15.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
