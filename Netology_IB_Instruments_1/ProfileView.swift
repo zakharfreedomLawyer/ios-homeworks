055a263ef86d958f6a59c3c688a7744128f6a3e6
zakharfreedomLawyer <107808581+zakharfreedomLawyer@users.noreply.github.com>
2022-09-27 18:56:54 +0600

    first commit

---
+++ Netology_IB_Instruments_1/ProfileView.swift
@@ -0,0 +1,29 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments_1
+//
+//  Created by Захар Кисляк on 15.09.2022.
+//
+
+import UIKit
+
+class ProfileView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Netology_IB_Instruments_1/ProfileViewController.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileViewController.swift
+//  Netology_IB_Instruments_1
+//
+//  Created by Захар Кисляк on 16.09.2022.
+//
+
+import Foundation
