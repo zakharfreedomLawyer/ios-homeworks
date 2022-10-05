9abfc073624df864073d0c6272b0bb361c00545c
zakharfreedomLawyer <107808581+zakharfreedomLawyer@users.noreply.github.com>
2022-10-05 16:04:37 +0500

    create new branch ios-developer

--- Netology_IB_Instruments_1/ProfileViewController.swift
+++ Netology_IB_Instruments_1/ProfileViewController.swift
@@ -5,4 +5,27 @@
 //  Created by Захар Кисляк on 16.09.2022.
 //
 
-import Foundation
+import UIKit
+
+class ProfileViewController: UIView {
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        self.setUp()
+    }
+    
+    private func setUp () {
+        let view = self.loadViewFromXib()
+       self.addSubview(view)
+       view.frame = self.bounds
+    }
+    
+    private func loadViewFromXib () -> UIView {
+        guard let view = Bundle.main.loadNibNamed("ProfileView", owner: ProfileView().self)?.first as? UIView else {
+            return UIView()
+        } 
+           
+        return view
+    }
+}
+
